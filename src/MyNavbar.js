3a62ad75b568e67719e6db4b94619f465d10d27f
Szymon <s17655@pjwstk.edu.pl>
2020-12-30 13:48:36 +0100

    Added navbar logged in/out logic

--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -23,10 +23,29 @@ class MyNavbar extends React.Component {
       isTeacher: true,
       isAdmin: true,
     };
+    this.logOut = this.logOut.bind(this);
+    this.logIn = this.logIn.bind(this);
+  }
+
+  logOut() {
+    this.setState((state) => ({
+      isLoggedIn: false,
+      login: null,
+      isTeacher: false,
+      isAdmin: false,
+    }));
+  }
+
+  logIn() {
+    this.setState((state) => ({
+      isLoggedIn: true,
+      //login: null,
+      //isTeacher: false,
+      //isAdmin: false,
+    }));
   }
 
   render() {
-
     return (
       <div>
         <Navbar expand="md" color="dark" dark>
@@ -34,46 +53,54 @@ class MyNavbar extends React.Component {
           <NavbarToggler />
           <Collapse isOpen="true" navbar>
             <Nav className="mr-auto" navbar>
-              {this.state.isLoggedIn &&
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Student
-                </DropdownToggle>
-                <DropdownMenu left>
-                  <DropdownItem>My webinars</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              }
-              {this.state.isLoggedIn && this.state.isTeacher &&
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Teacher
-                </DropdownToggle>
-                <DropdownMenu left>
-                  <DropdownItem>My webinars</DropdownItem>
-                  <DropdownItem>Add Webinar</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              }
-              {this.state.isLoggedIn && this.state.isAdmin &&
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Admin
-                </DropdownToggle>
-                <DropdownMenu left>
-                  <DropdownItem>Users</DropdownItem>
-                  <DropdownItem>Add User</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              }
+              {this.state.isLoggedIn && (
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Student
+                  </DropdownToggle>
+                  <DropdownMenu left>
+                    <DropdownItem>My webinars</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              )}
+              {this.state.isLoggedIn && this.state.isTeacher && (
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Teacher
+                  </DropdownToggle>
+                  <DropdownMenu left>
+                    <DropdownItem>My webinars</DropdownItem>
+                    <DropdownItem>Add Webinar</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              )}
+              {this.state.isLoggedIn && this.state.isAdmin && (
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Admin
+                  </DropdownToggle>
+                  <DropdownMenu left>
+                    <DropdownItem>Users</DropdownItem>
+                    <DropdownItem>Add User</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              )}
               <NavItem>
                 <NavLink href="#">Contact</NavLink>
               </NavItem>
             </Nav>
-            {this.state.isLoggedIn && <Button color="warning">Log out</Button>}
+            {this.state.isLoggedIn && (
+              <Button color="warning" onClick={this.logOut}>
+                Log out
+              </Button>
+            )}
             {!this.state.isLoggedIn && <Button color="info">Sign up</Button>}
             {"'"}
-            {!this.state.isLoggedIn && <Button color="success">Log in</Button>}
+            {!this.state.isLoggedIn && (
+              <Button color="success" onClick={this.logIn}>
+                Log in
+              </Button>
+            )}
             {"'"}
           </Collapse>
         </Navbar>
